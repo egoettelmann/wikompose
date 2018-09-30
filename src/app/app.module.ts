19da4df619b035c382ee0cae21f34ff28d4a91a2
Elio Goettelmann <elio.goettelmann@gmail.com>
2018-09-30 18:35:33 +0200

    Added ngx-markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient })
   ],
   providers: [],
   bootstrap: [AppComponent]
